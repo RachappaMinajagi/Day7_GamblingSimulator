e69e6c8ed0a7d3b78164543edfd80c506f86f945
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-02-19 10:11:34 +0530

    Gambler Simulator Problem

---
+++ src/Gambler.java
@@ -0,0 +1,15 @@
+public class Gambler {
+	
+	//UC1
+	public static final int INITIAL_BET_STAKE = 100;
+    public static final int BET_AMT = 1;
+    
+    
+    
+	public static void main(String[] args) {
+		System.out.println("welcome to gambler simulator problem");
+		Gambler game = new Gambler();
+	
+	}
+
+}
