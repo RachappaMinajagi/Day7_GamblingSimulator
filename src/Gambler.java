68de9486ab267bcf3f164c7f71de565822407f94
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-19 11:50:25 +0530

    Displaying welcome to Gambler Simulator Problem

--- src/Gambler.java
+++ src/Gambler.java
@@ -4,7 +4,6 @@ public static final int INITIAL_BET_STAKE = 100;
     
 	public static void main(String[] args) {
 		System.out.println("welcome to gambler simulator problem");
-		Gambler game = new Gambler();
 	
 	}
 
