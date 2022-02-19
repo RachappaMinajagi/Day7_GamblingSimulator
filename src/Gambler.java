a1e0460e2b6a0553cba4710d84ee1df2262e222f
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-19 11:36:58 +0530

    UC1 is Added

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,10 +1,8 @@
 public class Gambler {
 	
-	//UC1
+	//UC1  As a Gambler, would start with a stake
 	public static final int INITIAL_BET_STAKE = 100;
     public static final int BET_AMT = 1;
-    
-    
     
 	public static void main(String[] args) {
 		System.out.println("welcome to gambler simulator problem");
