741d22e28534f5aef93f9e97b875ced4ded1a6a3
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-19 11:43:50 +0530

    UC3 is Added Gambler Simulator Problem

--- src/Gambler.java
+++ src/Gambler.java
@@ -2,12 +2,16 @@ import java.util.Scanner;
 
 public class Gambler {
 
-	// UC1 As a Gambler, would start with a stake
+	/* UC1 As a Gambler, would start with a stake
+	 * 
+	 */
 	public static final int INITIAL_BET_STAKE = 100;
 	public static final int BET_AMT = 1;
 	Scanner.scanner = new Scanner (System .in);
 
-	// UC2 As a Gambler make bet so either win or loose
+	/* UC2 As a Gambler make bet so either win or loose
+	 * 
+	 */
 	public void gambler_SolutionUC2() {
 
 		double num = Math.floor(Math.random() * 10) % 2;
