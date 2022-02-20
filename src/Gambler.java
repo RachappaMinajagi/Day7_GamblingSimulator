3a7cdac5f85c59a7c007f72f4d923b0ec1c25234
RachappaMinajagi <59776797+RachappaMinajagi@users.noreply.github.com>
2022-02-20 22:53:01 +0530

    Added :  if Gambler won or lost

--- src/Gambler.java
+++ src/Gambler.java
@@ -1,55 +1,50 @@
-import java.util.Scanner;
+
+import java.util.Scanner; // import scanner class
 
 public class Gambler {
 
-	/* UC1 As a Gambler, would start with a stake
-	 * 
-	 */
-	public static final int INITIAL_BET_STAKE = 100;
-	public static final int BET_AMT = 1;
-	Scanner.scanner = new Scanner (System .in);
 
-	/* UC2 As a Gambler make bet so either win or loose
-	 * 
-	 */
-	public void gambler_SolutionUC2() {
+    static int AMOUNT_OF_STAKE = 100;
+    static int BET_AMOUNT = 1;
+    static int WON = 1;
+    static int LOST = 0;
 
-		double num = Math.floor(Math.random() * 10) % 2;
-		int remg_amt = INITIAL_BET_STAKE;
-		if (num == 0) {
-			remg_amt += BET_AMT;
-			System.out.println("The Gambler wins the bet and he has $" + remg_amt + " with him after one bet.");
-		} else {
-			remg_amt -= BET_AMT;
-			System.out.println("The Gambler loses the bet and he has $" + remg_amt + " with him after one bet.");
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Starting amount of stake is " + AMOUNT_OF_STAKE);
+        System.out.println("Amount to bet after every play " + BET_AMOUNT);
+        
+	    int cashInHand = AMOUNT_OF_STAKE;
+            int InHandcash = 0;
+             
+	    while (InHandcash > 50 && InHandcash < 150) {
+            int play = (int) Math.floor(Math.random() * 10) % 2;
+            System.out.println("Gamblers play:" + play);
 
-	/*
-	 * UC3: As a Gambler make bet so either win or loose
-	 */
-	public void gambler_SolutionUC3(int margin_perct) {
-		double uppr_margin = INITIAL_BET_STAKE + margin_perct * INITIAL_BET_STAKE / 100;
-		double lowr_margin = INITIAL_BET_STAKE - margin_perct * INITIAL_BET_STAKE / 100;
-		double avail_amt = INITIAL_BET_STAKE;
-		while ((avail_amt < uppr_margin) && (avail_amt > lowr_margin)) {
 
-			double num1 = Math.floor(Math.random() * 10) % 2;
+            switch (play) {  // Switch statement Uses 
+                case WON:
+                    cashInHand = InHandcash + BET_AMOUNT;
+                    System.out.println("Total current cash gambler has " + InHandcash);
+                    break;
+                case LOST:
+                    InHandcash = InHandcash - BET_AMOUNT;
+                    System.out.println("Total current cash gambler has " + InHandcash);
+                    break;
+                default:
+                    System.out.println("default");
+            }
+            if (play == 1) {
+                System.out.println("Player won the current play");
+            } else {
+                System.out.println("Player lost the current play");
+            }
 
-			if (num1 == 0) {
-				avail_amt += BET_AMT;
-			} else {
-				avail_amt -= BET_AMT;
-			}
-		}
-		System.out.println("The gambler resigns for the day after he has $" + avail_amt + " with him.");
-	}
+        }
+        if (InHandcash==150 || InHandcash==50) {
+            System.out.println("Player will resign for current day");
 
-	public static void main(String[] args) {
-		System.out.println("welcome to gambler simulator problem");
-		Gambler game = new Gambler();
-
-		game.gambler_SolutionUC3(10);
-	}
+        }
+    }
 
 }
